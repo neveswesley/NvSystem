f86f637b3a5c93b3d020a905e3b67fc734f37aa4
wesley neves <wellynvs@gmail.com>
2026-01-02 20:33:23 -0300

    feat(Product): Product Controller

---
+++ BackEnd/src/NvSystem.API/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NvSystem.Application.UseCases.Category.Commands;
+using NvSystem.Application.UseCases.Category.Query;
+using NvSystem.Domain.DTOs;
+using NvSystem.Domain.Entities;
+
+namespace NvSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateCategoryCommand command, CancellationToken cancellationToken)
+        {
+            var categoryId = await _mediator.Send(command, cancellationToken);
+            return CreatedAtAction(nameof(GetById), new {id = categoryId}, new {id = categoryId});
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put([FromRoute] Guid id, UpdateCategoryDto dto,
+            CancellationToken cancellationToken)
+        {
+            await _mediator.Send(new UpdateCategoryCommand(id, dto), cancellationToken);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(new DeleteCategoryCommand(id), cancellationToken);
+            return NoContent();
+        }
+
+        [HttpPatch("DisableCategory/{id}")]
+        public async Task<ActionResult> DisableCategory([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(new DisableCategoryCommand(id), cancellationToken);
+            return NoContent();
+        }
+
+        [HttpGet("GetAllCategories")]
+        public async Task<ActionResult> GetAllCategories(CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetAllCategoryQuery(), cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAllActiveCategories")]
+        public async Task<ActionResult> GetAllActiveCategories(CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetAllActiveCategoriesQuery(), cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpGet("GetCategoryById/{id}")]
+        public async Task<ActionResult> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetCategoryByIdQuery(id), cancellationToken);
+            return Ok(result);
+        }
+    }
+}
---
+++ BackEnd/src/NvSystem.API/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NvSystem.Application.UseCases.Product.Commands;
+using NvSystem.Application.UseCases.Product.Queries;
+
+namespace NvSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommand command, CancellationToken ct)
+        {
+            var result = await _mediator.Send(command, ct);
+            return CreatedAtAction(nameof(GetById), new {id = result}, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken ct)
+        {
+            var result = await _mediator.Send(new GetAllProductsQuery(), ct);
+            return Ok(result);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken ct)
+        {
+            var result = await _mediator.Send(new GetProductByIdQuery(id), ct);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken ct)
+        {
+            await _mediator.Send(new DeleteProductCommand(id), ct);
+            return NoContent();
+        }
+
+        [HttpPatch("/DisableProduct/{id}")]
+        public async Task<IActionResult> DisableProduct([FromRoute] Guid id, CancellationToken ct)
+        {
+            await _mediator.Send(new DisableProductCommand(id), ct);
+            return NoContent();
+        }
+
+        [HttpPatch("/EnableProduct/{id}")]
+        public async Task<IActionResult> EnableProduct([FromRoute] Guid id, CancellationToken ct)
+        {
+            //await _mediator.Send(new ActiveProductCommand(id), ct);
+            return NoContent();
+        }
+
+        [HttpGet("/GetLookupProduct/{barcode}")]
+        public async Task<IActionResult> GetLookupProduct([FromRoute] string barcode, CancellationToken ct)
+        {
+            var product = await _mediator.Send(new GetLookupByBarcode(barcode));
+            return Ok(product);
+        }
+        
+    }
+}
